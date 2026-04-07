c090edd619dfac0038de11da74824d3a88a91f10
jaswanthkumarck-eng <jaswanthkumar.ck@gmail.com>
2026-04-07 21:41:29 +0530

    UC1

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        List<String> bogies = new ArrayList<>();
+
+        System.out.println("Train consist initialized.");
+        System.out.println("Initial number of bogies: " + bogies.size());
+
+        System.out.println("System ready for further operations.");
+    }
 }
